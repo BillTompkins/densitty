e5ed6a0c2f42b300695443005e1684e2f6831fac
Bill Tompkins <204918+BillTompkins@users.noreply.github.com>
2026-01-15 17:51:54 -0500

    Add mock for color detection, tweak screensize mock

--- tests/helper_tests.py
+++ tests/helper_tests.py
@@ -1,6 +1,7 @@
 import os
 import pytest
 import random
+from unittest import mock
 
 from densitty import axis, binning, detect
 
@@ -16,35 +17,43 @@ def mock_get_terminal_size():
 
 
 @pytest.fixture()
+def force_truecolor(monkeypatch):
+    monkeypatch.setenv("FORCE_COLOR", "3")
+
+
+@pytest.fixture()
+def set_screensize(monkeypatch):
+    monkeypatch.setattr(os, "get_terminal_size", mock_get_terminal_size)
+
+
+@pytest.fixture()
 def points():
     """Example data"""
     random.seed(1)
     return [(random.triangular(-10, 10, 2), random.gauss(-1, 2)) for _ in range(10000)]
 
 
-def test_histplot2d_1(points):
+def test_histplot2d_1(points, force_truecolor):
     """Simplest usage"""
     plt = detect.histplot2d(points)
     plt.show()
     golden.check(plt.as_strings())
 
 
-def test_histplot2d_2(points):
+def test_histplot2d_2(points, force_truecolor):
     """40x40 bins, Tell it the data is ranging from -10..10"""
     plt = detect.histplot2d(points, 40, ((-10, 10), (-10, 10)))
     plt.show()
     golden.check(plt.as_strings())
 
 
-def test_histplot2d_3(points, monkeypatch):
-    monkeypatch.setattr(os, "get_terminal_size", mock_get_terminal_size)
+def test_histplot2d_3(points, force_truecolor, set_screensize):
     plt = detect.histplot2d(points, scale=True)
     plt.show()
     golden.check(plt.as_strings())
 
 
-def test_histplot2d_4(points, monkeypatch):
-    monkeypatch.setattr(os, "get_terminal_size", mock_get_terminal_size)
+def test_histplot2d_4(points, force_truecolor, set_screensize):
     plt = detect.histplot2d(points, scale=5)
     plt.show()
     golden.check(plt.as_strings())
